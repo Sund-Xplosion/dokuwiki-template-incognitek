3ebc1721b513f5c47d9716fe228eadf64e1e0158
Sven Fischer <git-dev@linux4tw.de>
2012-10-23 23:40:39 +0200

    adjusted menu buttons

--- menu.php
+++ menu.php
@@ -1,6 +1,12 @@
 <ul>
-  <li><?php tpl_link(wl("start"),"start"); ?></li>
-  <li><?php tpl_link(wl("blog"),"blog"); ?></li>
-  <li><?php tpl_link(wl("user:name"),"personal"); ?></li>
-  <li><?php tpl_link(wl("projects:projects"),"projects"); ?></li>
+  <li><?php tpl_link(wl("public:start"),"home"); ?></li>
+  <li><?php tpl_link(wl("start"),"intern"); ?></li>
+  <li><?php tpl_link(wl("protokolle:start"),"protokolle"); ?></li>
+  <li><?php tpl_link("?do=recent","recent changes"); ?></li>
+  <li><?php tpl_link("?do=backlink","backlinks"); ?></li>
+  <li><?php tpl_link("?do=index","index"); ?></li>
+  <li><?php tpl_link("?do=admin&page=pagemove","move"); ?></li>
+<!--  <li><?php #tpl_link(wl("blog"),"blog"); ?></li>
+  <li><?php #tpl_link(wl("user:name"),"personal"); ?></li>
+  <li><?php #tpl_link(wl("projects:projects"),"projects"); ?></li>-->
 </ul>
