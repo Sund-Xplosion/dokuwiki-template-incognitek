05266e35d1ddb09579ce3faed19f6eba9754851c
Sven Fischer <sven.fischer@sund-xplosion.de>
2013-02-25 03:36:01 +0100

    added link to sx pad in menu and deactivated move button

--- menu.php
+++ menu.php
@@ -2,11 +2,12 @@
   <li><?php tpl_link(wl("public:start"),"home"); ?></li>
   <li><?php tpl_link(wl("start"),"intern"); ?></li>
   <li><?php tpl_link(wl("protokolle:start"),"protokolle"); ?></li>
+  <li><?php tpl_link("https://pad.sund-xplosion.de","pad"); ?></li>
   <li><?php tpl_link("?do=recent","recent changes"); ?></li>
   <li><?php tpl_link("?do=backlink","backlinks"); ?></li>
   <li><?php tpl_link("?do=index","index"); ?></li>
-  <li><?php tpl_link("?do=admin&page=pagemove","move"); ?></li>
-<!--  <li><?php #tpl_link(wl("blog"),"blog"); ?></li>
+<!--   <li><?php tpl_link("?do=admin&page=pagemove","move"); ?></li>
+  <li><?php #tpl_link(wl("blog"),"blog"); ?></li>
   <li><?php #tpl_link(wl("user:name"),"personal"); ?></li>
   <li><?php #tpl_link(wl("projects:projects"),"projects"); ?></li>-->
 </ul>
